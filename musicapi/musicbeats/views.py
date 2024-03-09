5ab814d00a772260bfb4ba6a7164a25ef7b58d26
DHAIRYAKARSHINI <2200031455@kluniversity.in>
2024-03-09 14:38:25 +0530

    PFSD Project uploaded

---
+++ musicapi/musicbeats/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.shortcuts import redirect
+from django.db.models import Case,When
+from django.shortcuts import render
+
+from .models import Song,WatchLater
+def index(request):
+    song=Song.objects.all
+    return render(request,'index.html',{'song':song})
+
+def songs(request):
+    song = Song.objects.all
+    return render(request,'musicbeats/songs.html',{'song':song})
+
+def songpost(request,id):
+    song= Song.objects.filter(song_id=id).first()
+    return render(request, 'musicbeats/songpost.html', {'song': song})
+
+
+def login(request):
+    if request.method == "POST":
+
+        username = request.POST['username']
+
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        from django.contrib.auth import login
+        login(request, user)
+        return redirect('/')
+
+    return render(request, 'musicbeats/login.html')
+
+def signup(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        username = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.first_name=first_name
+        myuser.first_name = last_name
+        myuser.save()
+        user = authenticate(username=username,password=pass1)
+        from django.contrib.auth import login
+        login(request,user)
+        return redirect('/')
+    return render(request, 'musicbeats/signup.html')
+
+def watchlater(request):
+    if request.method == "POST":
+        user= request.user
+        video_id=request.POST['video_id']
+        cond = True
+        watch = WatchLater.objects.filter(user=user)
+
+        for i in watch:
+            if video_id== i.video_id:
+                message = "Your Video is already added"
+                break
+        else:
+            watchlater=WatchLater(user=user,video_id=video_id)
+            watchlater.save()
+            message="Your video is Successfully added"
+        song = Song.objects.filter(song_id=video_id).first()
+        return redirect(f"/musicbeats/songs/{video_id}",{"message":message})
+    wl = WatchLater.objects.filter(user=request.user)
+    ids=[]
+    for i in wl:
+        ids.append(i.video_id)
+    preserved = Case(*[When(pk=pk,then=pos) for pos,pk in enumerate(ids)])
+    song=Song.objects.filter(song_id__in=ids).order_by(preserved)
+
+    return  render(request,'musicbeats/watchlater.html',{'song':song})
